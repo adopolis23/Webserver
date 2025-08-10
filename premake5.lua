a9a754a1e96ce4e28d94e29aa2901ee9e0340a00
adopolis23 <weinhofer@usf.edu>
2025-08-10 15:58:36 -0400

    Initial Commit, premake file

---
+++ premake5.lua
@@ -0,0 +1,20 @@
+workspace "Webserver"
+    configurations {"Debug", "Release"}
+    architecture "x86_64"
+    startproject "Webserver"
+
+project "Webserver"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "bin-int/%{cfg.buildcfg}"
+
+    files { "src/**.cpp" }
+    includedirs { "src" }
+
+    filter "configurations:Debug"
+    symbols "On"
+
+    filter "configurations:Release"
+        optimize "On"
